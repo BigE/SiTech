b979594a150478d55c485d1e0923a0ff454de2b0
Eric Gach <eric@php-oop.net>
2007-04-26 07:01:49 +0000

    Added full error control support for MySQL DB. Also converted spaces to tabs.

--- SiTech/DB/MySQL.php
+++ SiTech/DB/MySQL.php
@@ -25,143 +25,143 @@ class SiTech_DB_MySQL extends SiTech_DB_Base
 	 *
 	 * @return bool
 	 */
-    public function beginTransaction()
-    {
-        if ($this->prepare('BEGIN')->execute() === false) {
-            return(false);
-        } else {
-            return(true);
-        }
-    }
+	public function beginTransaction()
+	{
+		if ($this->prepare('BEGIN')->execute() === false) {
+			return(false);
+		} else {
+			return(true);
+		}
+	}
 
-    /**
-     * Commit (finish) a SQL transaction.
-     *
-     * @return bool
-     */
-    public function commit()
-    {
-        if ($this->prepare('COMMIT')->execute() === false) {
-            return(false);
-        } else {
-            return(true);
-        }
-    }
+	/**
+	 * Commit (finish) a SQL transaction.
+	 *
+	 * @return bool
+	 */
+	public function commit()
+	{
+		if ($this->prepare('COMMIT')->execute() === false) {
+			return(false);
+		} else {
+			return(true);
+		}
+	}
 
-    /**
-     */
-    public function errorCode()
-    {
-    }
+	/**
+	 */
+	public function errorCode()
+	{
+	}
 
-    /**
-     * Get the last error number and string from the database.
-     *
-     * @return array
-     */
-    public function errorInfo()
-    {
-    	return(array(
-    		mysql_errno(),
-    		mysql_error()
-    	));
-    }
+	/**
+	 * Get the last error number and string from the database.
+	 *
+	 * @return array
+	 */
+	public function errorInfo()
+	{
+		return(array(
+			@mysql_errno($this->_conn),
+			@mysql_error($this->_conn)
+		));
+	}
 
-    /**
-     * Get the last ID inserted into the row. If the row name is not present, we just
-     * attempt to get the ID from the insert_id function.
-     *
-     * @param string $name Row name to get value from.
-     * @return mixed
-     * @todo Need to add functionality to grab the specified column from the table.
-     */
-    public function lastInsertId($name=null)
-    {
-    	if (is_null($name)) {
-    		return(mysql_insert_id());
-    	} else {
-    		/* TODO: Finish this part */
-    	}
-    }
+	/**
+	 * Get the last ID inserted into the row. If the row name is not present, we just
+	 * attempt to get the ID from the insert_id function.
+	 *
+	 * @param string $name Row name to get value from.
+	 * @return mixed
+	 * @todo Need to add functionality to grab the specified column from the table.
+	 */
+	public function lastInsertId($name=null)
+	{
+		if (is_null($name)) {
+			return(@mysql_insert_id($this->_conn));
+		} else {
+			/* TODO: Finish this part */
+		}
+	}
 
-    /**
-     * Prepare a SQL string for execution. This does not execute the query yet.
-     *
-     * @param string $sql SQL string to prepare.
-     * @param array $args Additional arguments to use in the query.
-     * @return SiTech_DB_Statement_MySQL
-     */
-    public function prepare($sql, $args=array())
-    {
-        if (!is_resource($this->_conn)) {
-            $this->_connect();
-        }
+	/**
+	 * Prepare a SQL string for execution. This does not execute the query yet.
+	 *
+	 * @param string $sql SQL string to prepare.
+	 * @param array $args Additional arguments to use in the query.
+	 * @return SiTech_DB_Statement_MySQL
+	 */
+	public function prepare($sql, $args=array())
+	{
+		if (!is_resource($this->_conn)) {
+			$this->_connect();
+		}
 
-        SiTech::loadClass('SiTech_DB_Statement_MySQL');
-        $stmnt = new SiTech_DB_Statement_MySQL($sql, $this->_conn, $args);
-        return($stmnt);
-    }
+		SiTech::loadClass('SiTech_DB_Statement_MySQL');
+		$stmnt = new SiTech_DB_Statement_MySQL($sql, $this->_conn, $args);
+		return($stmnt);
+	}
 
-    /**
-     * Add quotes to a value so it is safe to be used in the SQL query.
-     *
-     * @param string $string Value to add quotes too.
-     * @param unknown_type $mode
-     * @return string
-     */
-    public function quote($string, $mode=null)
-    {
-        return(mysql_real_escape_string($string));
-    }
+	/**
+	 * Add quotes to a value so it is safe to be used in the SQL query.
+	 *
+	 * @param string $string Value to add quotes too.
+	 * @param unknown_type $mode
+	 * @return string
+	 */
+	public function quote($string, $mode=null)
+	{
+		return(mysql_real_escape_string($string));
+	}
 
-    /**
-     * Rollback (forget) changes made by transaction to the database.
-     *
-     * @return bool
-     */
-    public function rollBack()
-    {
-        if ($this->prepare('ROLLBACK')->execute() === false) {
-            return(false);
-        } else {
-            return(true);
-        }
-    }
+	/**
+	 * Rollback (forget) changes made by transaction to the database.
+	 *
+	 * @return bool
+	 */
+	public function rollBack()
+	{
+		if ($this->prepare('ROLLBACK')->execute() === false) {
+			return(false);
+		} else {
+			return(true);
+		}
+	}
 
-    /**
-     * Make a connection to the database.
-     *
-     * @throws SiTech_DB_Exception
-     */
-    protected function _connect()
-    {
-    	if (isset($this->_dsn['port'])) {
-    		$host = $this->_dsn['host'].':'.$this->_dsn['port'];
-    	} else {
-    		$host = $this->_dsn['host'];
-    	}
+	/**
+	 * Make a connection to the database.
+	 *
+	 * @throws SiTech_DB_Exception
+	 */
+	protected function _connect()
+	{
+		if (isset($this->_dsn['port'])) {
+			$host = $this->_dsn['host'].':'.$this->_dsn['port'];
+		} else {
+			$host = $this->_dsn['host'];
+		}
 
-        if (($this->_conn = mysql_connect($host, $this->_dsn['user'], $this->_dsn['pass'])) === false) {
-            SiTech::loadClass('SiTech_DB_Exception');
-            throw new SiTech_DB_Exception('Failed to connect to MySQL database.%sMySQL reported: (%d) %s', array("\n", mysql_errno(), mysql_error()));
-        }
+		if (($this->_conn = @mysql_connect($host, $this->_dsn['user'], $this->_dsn['pass'])) === false) {
+			SiTech::loadClass('SiTech_DB_Exception');
+			throw new SiTech_DB_Exception('Failed to connect to MySQL database.%sMySQL reported: (%d) %s', array("\n", mysql_errno(), mysql_error()));
+		}
 
-        if (mysql_select_db($this->_dsn['dbname']) === false) {
-        	SiTech::loadClass('SiTech_DB_Exception');
-        	throw new SiTech_DB_Exception('Failed to select MySQL database "%s"%sMySQL reported: (%d) %s', array($this->_dsn['dbname'], "\n", mysql_errno(), mysql_error()));
-        }
-    }
+		if (@mysql_select_db($this->_dsn['dbname']) === false) {
+			SiTech::loadClass('SiTech_DB_Exception');
+			throw new SiTech_DB_Exception('Failed to select MySQL database "%s"%sMySQL reported: (%d) %s', array($this->_dsn['dbname'], "\n", mysql_errno(), mysql_error()));
+		}
+	}
 
-    /**
-     * Close the connection to the database if one is active.
-     */
-    protected function _disconnect()
-    {
-        if (is_resource($this->_conn)) {
-            mysql_close($this->_conn);
-        }
+	/**
+	 * Close the connection to the database if one is active.
+	 */
+	protected function _disconnect()
+	{
+		if (is_resource($this->_conn)) {
+			@mysql_close($this->_conn);
+		}
 
-        $this->_conn = null;
-    }
+		$this->_conn = null;
+	}
 }
 ?>
--- SiTech/DB/Statement/MySQL.php
+++ SiTech/DB/Statement/MySQL.php
@@ -23,101 +23,101 @@ class SiTech_DB_Statement_MySQL extends SiTech_DB_Statement_Base
 	/**
 	 * Close the database cursor and free the result.
 	 */
-    public function closeCursor()
-    {
-        mysql_free($this->_result);
-    }
+	public function closeCursor()
+	{
+		@mysql_free_result($this->_result);
+	}
 
-    /**
-     * Get the number of columns present in the current cursor result.
-     *
-     * @return int
-     */
-    public function columnCount()
-    {
-        return(mysql_num_fields($this->_result));
-    }
+	/**
+	 * Get the number of columns present in the current cursor result.
+	 *
+	 * @return int
+	 */
+	public function columnCount()
+	{
+		return(@mysql_num_fields($this->_result));
+	}
 
-    /**
-     * Get the ANSII-SQL error code returned by the server. Unsupported
-     * by the mysql extension.
-     */
-    public function errorCode()
-    {
-        /* unsupported by the MySQL extension */
-    }
+	/**
+	 * Get the ANSII-SQL error code returned by the server. Unsupported
+	 * by the mysql extension.
+	 */
+	public function errorCode()
+	{
+		/* unsupported by the MySQL extension */
+	}
 
-    /**
-     * Grab the most recent error number and string from the server.
-     *
-     * @return array
-     */
-    public function errorInfo()
-    {
-        return(array(
-            mysql_errno($this->_conn),
-            mysql_error($this->_conn)
-        ));
-    }
+	/**
+	 * Grab the most recent error number and string from the server.
+	 *
+	 * @return array
+	 */
+	public function errorInfo()
+	{
+		return(array(
+			@mysql_errno($this->_conn),
+			@mysql_error($this->_conn)
+		));
+	}
 
-    /**
-     * Execute the SQL statement that has been prepared.
-     *
-     * @param array $params Any parameters to set in the query itself.
-     */
-    public function execute($params=array())
-    {
-        if (!is_resource($this->_conn)) {
-            SiTech::loadClass('SiTech_DB_Statement_Exception');
-            throw new SiTech_DB_Statement_Exception('The connection to the database is not currently open.');
-        }
+	/**
+	 * Execute the SQL statement that has been prepared.
+	 *
+	 * @param array $params Any parameters to set in the query itself.
+	 */
+	public function execute($params=array())
+	{
+		if (!is_resource($this->_conn)) {
+			SiTech::loadClass('SiTech_DB_Statement_Exception');
+			throw new SiTech_DB_Statement_Exception('The connection to the database is not currently open.');
+		}
 
-        if (($this->_result = mysql_query($this->_sql)) === false) {
-            SiTech::loadClass('SiTech_DB_Statement_Exception');
-            throw new SiTech_DB_Statement_Exception('The query to the database failed.%sMySQL reported: (%d) %s', array("\n", mysql_errno(), mysql_error()));
-        }
-    }
+		if (($this->_result = @mysql_query($this->_sql)) === false) {
+			SiTech::loadClass('SiTech_DB_Statement_Exception');
+			throw new SiTech_DB_Statement_Exception('The query to the database failed.%sMySQL reported: (%d) %s', array("\n", mysql_errno(), mysql_error()));
+		}
+	}
 
-    /**
-     * Not implemented.
-     *
-     * @param unknown_type $column
-     */
-    public function getColumnMeta($column)
-    {
-    }
+	/**
+	 * Not implemented.
+	 *
+	 * @param unknown_type $column
+	 */
+	public function getColumnMeta($column)
+	{
+	}
 
-    /**
-     * Get the number of rows returned by the result.
-     *
-     * @return int
-     */
-    public function rowCount()
-    {
-        $count = 0;
-        if (($count = @mysql_affected_rows($this->_result)) === false) {
-            $count = @mysql_num_rows($this->_result);
-        }
+	/**
+	 * Get the number of rows returned by the result.
+	 *
+	 * @return int
+	 */
+	public function rowCount()
+	{
+		$count = 0;
+		if (($count = @mysql_affected_rows($this->_result)) === false) {
+			$count = @mysql_num_rows($this->_result);
+		}
 
-        return($count);
-    }
+		return($count);
+	}
 
-    /**
-     * Fetch a single row from the result and return it.
-     *
-     * @param int $offset Row number to grab from the result.
-     * @return array
-     */
-    protected function _fetch($offset)
-    {
-    	if (@mysql_data_seek($this->_result, $offset) === false) {
-    		/*SiTech::loadClass('SiTech_DB_Statement_Exception');
-    		throw new SiTech_DB_Statement_Exception('The specified row offset %d is invalid.', array($offset));*/
-    		return(false);
-    	}
+	/**
+	 * Fetch a single row from the result and return it.
+	 *
+	 * @param int $offset Row number to grab from the result.
+	 * @return array
+	 */
+	protected function _fetch($offset)
+	{
+		if (@mysql_data_seek($this->_result, $offset) === false) {
+			/*SiTech::loadClass('SiTech_DB_Statement_Exception');
+			throw new SiTech_DB_Statement_Exception('The specified row offset %d is invalid.', array($offset));*/
+			return(false);
+		}
 
-        /* we use fetch_array so that the real fetch method gets all the info it needs */
-        return(mysql_fetch_array($this->_result));
-    }
+		/* we use fetch_array so that the real fetch method gets all the info it needs */
+		return(@mysql_fetch_array($this->_result));
+	}
 }
 ?>
