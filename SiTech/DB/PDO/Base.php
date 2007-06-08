b528197cb3e1c6ab6f3d791f1ec140fe67930794
Eric Gach <eric@php-oop.net>
2007-06-08 18:46:01 +0000

    Adding PDO stuff

---
+++ SiTech/DB/PDO/Base.php
@@ -0,0 +1,8 @@
+<?php
+require_once('SiTech.php');
+SiTech::loadClass('SiTech_DB_Base');
+
+abstract class SiTech_DB_PDO_Base extends PDO
+{
+}
+?>
---
+++ SiTech/DB/PDO/MySQL.php
@@ -0,0 +1,8 @@
+<?php
+require_once('SiTech.php');
+SiTech::loadClass('SiTech_DB_PDO_Base');
+
+class SiTech_DB_PDO_MySQL extends SiTech_DB_PDO_Base
+{
+}
+?>
