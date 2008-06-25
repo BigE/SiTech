537dc6a06c2fc6781b93e84e8927cdb5c65e48ad
Eric Gach <eric@php-oop.net>
2008-06-25 15:37:25 +0000

    Lots of changes to documentation. Also adding the interface

---
+++ lib/SiTech/DB/Driver/Interface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Contains the interface for all database drivers.
+ *
+ * @author Eric Gach <eric@php-oop.net>
+ * @copyright SiTech Group (c) 2008
+ * @filesource
+ * @package SiTech_DB
+ * @subpackage SiTech_DB_Driver
+ * @version $Id$
+ */
+
+/**
+ * SiTech_DB_Driver_Interface - Interface for all database type classes.
+ *
+ * @package SiTech_DB_Driver
+ */
+interface SiTech_DB_Driver_Interface
+{
+	/**
+	 * Singleton method to get the instance of the driver.
+	 *
+	 * @return SiTech_DB_Driver_Interface
+	 */
+	static public function singleton();
+}
--- lib/SiTech/DB/Driver/SQLite.php
+++ lib/SiTech/DB/Driver/SQLite.php
@@ -1,8 +1,35 @@
 <?php
+/**
+ * Contains the driver for SQLite database connections.
+ *
+ * @author Eric Gach <eric@php-oop.net>
+ * @copyright SiTech Group (c) 2008
+ * @filesource
+ * @package SiTech_DB
+ * @subpackage SiTech_DB_Driver
+ * @version $Id$
+ */
+
+/**
+ * @see SiTech_DB_Driver_Abstract
+ */
 require_once('SiTech/DB/Driver/Abstract.php');
 
+/**
+ * SiTech_DB_Driver_SQLite - For use with SQLite databases.
+ *
+ * Driver that contains special methods and instructions for SQLite database
+ * connections.
+ *
+ * @package SiTech_DB_Driver
+ */
 class SiTech_DB_Driver_SQLite extends SiTech_DB_Driver_Abstract
 {
+	/**
+	 * Singleton method to get the instance of the driver.
+	 *
+	 * @return SiTech_DB_Driver_SQLite
+	 */
 	static public function singleton()
 	{
 		return(self::_singleton(__CLASS__));
