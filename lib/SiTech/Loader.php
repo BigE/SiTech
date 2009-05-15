6b2a23ba279a76ccad6add9a20254e818739ef16
Eric Gach <eric@php-oop.net>
2009-05-15 16:54:08 +0000

    Adding loader

---
+++ lib/SiTech/Loader.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Description of Loader.php
+ *
+ * @author Eric Gach <eric at php-oop.net>
+ * @copyright SiTech Group (c) 2009
+ * @filesource
+ * @package SiTech
+ * @version $Id$
+ */
+
+/** 
+ * Loader.php
+ * Copyright (C) 2009 Eric Gach <eric at php-oop.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Description of Loader
+ *
+ * @author Eric Gach <eric at php-oop.net>
+ */
+class SiTech_Loader
+{
+	public static function autoload($class)
+	{
+		self::loadClass($class);
+	}
+
+	public static function loadBootstrap($file = null)
+	{
+		if (empty($file)) {
+			if (!defined('SITECH_APP_PATH')) {
+				require_once('SiTech/Exception.php');
+				throw new SiTech_Exception('SITECH_APP_PATH not defined. Unable to detect path to find bootstrap file');
+			} else {
+				$file = SITECH_APP_PATH.'/bootstrap.php';
+			}
+		}
+
+		if (!is_readable($file)) {
+			require_once('SiTech/Exception.php');
+			throw new SiTech_Exception('Unable to load bootsrap file "%s"', array($file));
+		}
+
+		require($file);
+	}
+
+	public static function loadClass($class)
+	{
+		if (class_exists($class, false) || interface_exists($class, false)) {
+			return;
+		}
+
+		$file = str_replace('_', DIRECTORY_SEPARATOR, $class).'.php';
+		include_once($file);
+
+		if (!class_exists($class, false) || interface_exists($class, false)) {
+			require_once('SiTech/Exception.php');
+			throw new SiTech_Exception('The class "%s" failed to load', array($class));
+		}
+	}
+
+	public static function registerAutoload($class = 'SiTech_Loader', $enabled = true)
+    {
+        if (!function_exists('spl_autoload_register')) {
+            require_once 'SiTech/Exception.php';
+            throw new SiTech_Exception('spl_autoload does not exist in this PHP installation');
+        }
+
+        self::loadClass($class);
+        $methods = get_class_methods($class);
+        if (!in_array('autoload', (array)$methods)) {
+            require_once 'SiTech/Exception.php';
+            throw new SiTech_Exception('The class "%s" does not have an autoload() method', array($class));
+        }
+
+        if ($enabled === true) {
+            spl_autoload_register(array($class, 'autoload'));
+        } else {
+            spl_autoload_unregister(array($class, 'autoload'));
+        }
+    }
+}
