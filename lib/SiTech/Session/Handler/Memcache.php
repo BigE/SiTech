af19f1929da653f43baa9f83306e6f0140e78143
Eric Gach <eric@php-oop.net>
2010-12-07 16:44:23 +0000

    No clue if this will work for memcache.

--- lib/SiTech/Session/Handler/Memcache.php
+++ lib/SiTech/Session/Handler/Memcache.php
@@ -9,37 +9,83 @@
  *
  * @author eric
  */
-class SiTech_Session_Handler_Memcache extends Memcache
+class SiTech_Session_Handler_Memcache implements SiTech_Session_Handler_Interface
 {
-	public function __construct()
+	/**
+	 *
+	 * @var Memcache
+	 */
+	protected $_memcache;
+
+	public function __construct(Memcache $memcache = null)
 	{
-		ini_set('session.save_handler', 'SiTech_Session_Handler_Memcache');
+		if (empty($memcache) && ini_get('session.save_handler') == 'memcache') {
+			$memcache = new Memcache();
+			$memcache->connect(ini_get('session.save_path'));
+		} elseif (empty($memcache) || $memcache->getversion() === false) {
+			throw new SiTech_Exception('Cannot auto detect memcache settings. Please send an already connected object to this handler.');
+		}
+
+		// By default memchache sets the save_handler to memcache
+		ini_set('session.save_handler', 'user');
+		$this->_memcache = $memcache;
 	}
 
-	public function get($keys, $flags = null)
+	/**
+	 * Nothing to do here because we don't need to close memcache just because
+	 * the session has ended.
+	 *
+	 * @return bool
+	 */
+	public function close()
 	{
-		$data = parent::get($keys, $flags);
-		if (is_array($data)) {
-			array_walk($data, array($this, '_parseData'));
-		} else {
-			$this->_parseData($data);
-		}
+		return(true);
+	}
+
+	/**
+	 * Destroy teh sessions!
+	 *
+	 * @param string $id Session ID
+	 */
+	public function destroy($id)
+	{
+		return($this->_memcache->delete('session/'.$id));
+	}
+
+	/**
+	 * Cleanup for old sessions
+	 *
+	 * @param int $maxLife
+	 */
+	public function gc($maxLife)
+	{
+		return(true);
+	}
+
+	/**
+	 * Nothing to do here because memcache should already be opened in our
+	 * constructor.
+	 *
+	 * @return bool
+	 */
+	public function open()
+	{
+		return(true);
+	}
+
+	public function read($id)
+	{
+		$data = $this->_memcache->get('session/'.$id);
+		list($r, $s, $data) = explode("\n", $data);
+		$session = SiTech_Session::singleton();
 
 		return($data);
 	}
 
-	public function set($key, $var, $flag = null, $expire = null)
+	public function write($id, $data)
 	{
 		$session = SiTech_Session::singleton();
-		$var = sprintf("%d\n%d\n%s", $session->getAttribute(SiTech_Session::ATTR_REMEMBER), $session->getAttribute(SiTech_Session::ATTR_STRICT), $var);
-		return(parent::set($key, $var, $flag, $expire));
-	}
-
-	protected function _parseData(&$data, $key = null)
-	{
-		list($r, $s, $data) = explode("\n", $data, 3);
-		$session = SiTech_Session::singleton();
-		$session->setAttribute(SiTech_Session::ATTR_REMEMBER, (bool)$r);
-		$session->setAttribute(SiTech_Session::ATTR_STRICT, (bool)$s);
+		$data = sprintf("%d\n%d\n%s", $session->getAttribute(SiTech_Session::ATTR_REMEMBER), $session->getAttribute(SiTech_Session::ATTR_STRICT), $data);
+		return($this->_memcache->set('session/'.$id, $data));
 	}
 }
