535417291476c491940c01f629b65eafd4e4ca5d
Eric Gach <eric@php-oop.net>
2011-02-07 19:23:32 +0000

    Moving phpdoc shit around... no changes to code.

--- lib/SiTech/Controller.php
+++ lib/SiTech/Controller.php
@@ -15,19 +15,16 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * @author Eric Gach <eric@php-oop.net>
- * @copyright SiTech Group (c) 2008-2009
- * @filesource
- * @package SiTech
- * @subpackage SiTech_Controller
- * @version $Id$
  */
 
 /**
  * Description of Controller
  *
+ * @author Eric Gach <eric@php-oop.net>
+ * @copyright SiTech Group (c) 2008-2009
+ * @filesource
  * @package SiTech_Controller
+ * @version $Id$
  */
 class SiTech_Controller
 {
