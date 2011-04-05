bb9807e675701fd5fba27b107c85e398c4a7c549
Eric Gach <eric@php-oop.net>
2011-04-05 16:13:12 -0500

    Adding a setting to change xdebug.scream to off in case the xdebug extension is loaded and its enabled. It really reaks havok on the tests.

--- Tests/SiTech/SiTech_Test.php
+++ Tests/SiTech/SiTech_Test.php
@@ -1,4 +1,6 @@
 <?php
 define('SITECH_BASEPATH', realpath(dirname(__FILE__).'/../../'));
 set_include_path('.'.PATH_SEPARATOR.SITECH_BASEPATH.'/lib');
+// Turn scream off... this could cause problems with tests if we don't.
+ini_set('xdebug.scream', 0);
 ?>
