5e5cb2ac147bd102730ee02b376293b5dc811900
Eric Gach <eric@php-oop.net>
2011-04-07 10:17:01 -0500

    Convering CRLF to LF. Also changed Data field to be longtext instead of tinytext.

--- Tools/SiTech_Session.sql
+++ Tools/SiTech_Session.sql
@@ -1,10 +1,10 @@
 CREATE TABLE SiTech_Sessions (
   Name varchar(20) NOT NULL default '',
   Id varchar(32) NOT NULL default '',
-  Data tinytext NOT NULL,
+  Data longtext NOT NULL,
   Started timestamp NOT NULL default CURRENT_TIMESTAMP,
   Remember tinyint(1) NOT NULL default '0',
   Strict tinyint(1) NOT NULL default '0',
-  RemoteAddr varchar(15) NOT NULL default '',
+  RemoteAddr varchar(15) NULL default NULL,
   PRIMARY KEY  (Name,Id)
 );
