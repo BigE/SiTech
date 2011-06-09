b1443d971f87da191e1ffcccbd6e7bb6be5ad219
Eric Gach <eric@php-oop.net>
2011-06-09 13:50:20 -0500

    Changing variable names so they don't conflict with items inside the template. Of course, the template can override these still, but oh well if it does.

--- lib/SiTech/Template/Renderer/PHP.php
+++ lib/SiTech/Template/Renderer/PHP.php
@@ -35,7 +35,7 @@ require_once('SiTech/Template/Renderer/Exception.php');
  * @author Eric Gach <eric@php-oop.net>
  * @package SiTech\Template
  * @subpackage SiTech\Template\Renderer
- * @version $Id$
+ * @version $Id: 6fa70e42fe6166fbce9dc88a23bb34a2d0ecad34 $
  */
 class PHP implements IRenderer
 {
@@ -51,19 +51,19 @@ class PHP implements IRenderer
 	 * @param array $vars Variables that are set for the template
 	 * @return string Final output of template after rendered
 	 */
-	static public function render(\SiTech\Template\Engine $tpl, $file, $path, array $vars)
+	static public function render(\SiTech\Template\Engine $_SiTech_tpl, $_SiTech_file, $_SiTech_path, array $_SiTech_vars)
 	{
 		// Backup the old include path so we can reset it once we're done.
 		$_SiTech_oldPath = \set_include_path($path.\PATH_SEPARATOR.\get_include_path());
-		\extract($vars, \EXTR_OVERWRITE);
-		unset($vars);
+		\extract($_SiTech_vars, \EXTR_OVERWRITE);
+		unset($_SiTech_vars);
 
 		\ob_start();
-		@include($file);
+		@include($_SiTech_file);
 		$error = \error_get_last();
-		if (!empty($error) && $error['type'] == E_WARNING && preg_match('#Failed opening \''.$file.'\' for inclusion#', $error['message'])) {
+		if (!empty($error) && $error['type'] == E_WARNING && preg_match('#Failed opening \''.$_SiTech_file.'\' for inclusion#', $error['message'])) {
 			\ob_end_clean();
-			throw new Exception('Failed to open template file %s on path %s', array($file, $path));
+			throw new Exception('Failed to open template file %s on path %s', array($_SiTech_file, $_SiTech_path));
 		}
 		$content = \ob_get_clean();
 		\ob_start();
