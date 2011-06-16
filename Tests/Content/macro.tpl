9027046f6c9266373768814d149cabcf32894e50
Eric Gach <eric@php-oop.net>
2011-06-16 14:49:10 -0500

    Removing tests folder, swiching to PHPUnit for tests.

--- Tests/Content/macro.tpl
+++
@@ -1,1 +0,0 @@
-This is my {$test} page for Macro templates.
--- Tests/SiTech/SiTech_Test.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('SITECH_BASEPATH', realpath(dirname(__FILE__).'/../../'));
-set_include_path('.'.PATH_SEPARATOR.SITECH_BASEPATH.'/lib');
-// Turn scream off... this could cause problems with tests if we don't.
-ini_set('xdebug.scream', 0);
-?>
