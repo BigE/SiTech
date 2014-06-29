19f251f290f0d2ea9e666c6e768a104a62f47573
Eric Gach <eric@php-oop.net>
2014-06-29 03:15:51 -0500

    switching the dsn class to use the new container trait

--- src/SiTech/Database/Dsn.php
+++ src/SiTech/Database/Dsn.php
@@ -28,85 +28,28 @@
 
 namespace SiTech\Database
 {
-	abstract class Dsn implements \ArrayAccess
+	/**
+	 * Create a new DSN object with the config settings passed in.
+	 *
+	 * Passing in an array of config options will allow this class to
+	 * generate a usable DSN string to connect to the database with. Please
+	 * read the PDO Drivers documentation to see what options are available
+	 * for your driver.
+	 *
+	 * @see http://php.net/pdo.drivers
+	 */
+	abstract class Dsn
 	{
-		/**
-		 * This is the array of config items that are set on the object.
-		 *
-		 * @var array
-		 */
-		protected $_config = [];
+		use \SiTech\Helper\Container;
 
 		/**
-		 * This is the string version of the DSN that was loaded.
+		 * This is the string version of the DSN that was loaded. When the
+		 * _generateDsn method is called, it should store the result here.
 		 *
-		 * @see getDsn
+		 * @see getDsn, _generateDsn
 		 * @var string
 		 */
 		protected $_dsn;
-
-		/**
-		 * Create a new DSN object with the config settings passed in.
-		 *
-		 * Passing in an array of config options will allow this class to
-		 * generate a usable DSN string to connect to the database with. Please
-		 * read the PDO Drivers documentation to see what options are available
-		 * for your driver.
-		 *
-		 * @param array $config
-		 * @see http://php.net/pdo.drivers
-		 */
-		public function __construct(array $config)
-		{
-			$this->_config = $config;
-		}
-
-		/**
-		 * Allows access to config settings as properties of the object.
-		 *
-		 * The DSN object should return all configuration settings through this
-		 * method as properties of the object itself. This should make accessing
-		 * them easier.
-		 *
-		 * @param $name Name of the setting to get the value of
-		 * @return string|null
-		 */
-		public function __get($name)
-		{
-			if (isset($this->_config[$name])) {
-				return $this->_config[$name];
-			}
-
-			return null;
-		}
-
-		/**
-		 * Check if a config option is set by checking it as a property.
-		 *
-		 * This simply checks if the configuration item is set by returning the
-		 * value of isset()
-		 *
-		 * @param $name Name of the setting to check if is set
-		 * @return bool
-		 */
-		public function __isset($name)
-		{
-			return isset($this->_config[$name]);
-		}
-
-		/**
-		 * Set a configuration option to the specified value.
-		 *
-		 * This will simply set a value to a configuration option. There are no
-		 * checks in place for overwriting variables.
-		 *
-		 * @param $name
-		 * @param $value
-		 */
-		public function __set($name, $value)
-		{
-			$this->_config[$name] = $value;
-		}
 
 		/**
 		 * Return the compiled dsn string.
@@ -130,81 +73,6 @@ namespace SiTech\Database
 		{
 			$this->_generateDsn();
 			return $this->_dsn;
-		}
-
-		/**
-		 * Whether an offset exists in the config.
-		 *
-		 * This simply checks if a configuration option exists by using isset().
-		 *
-		 * @param mixed $offset
-		 * @return bool
-		 */
-		public function offsetExists($offset)
-		{
-			return isset($this->_config[$offset]);
-		}
-
-		/**
-		 * Retrieve an offset from the config.
-		 *
-		 * By setting $prefix to true it will return the offset name combined
-		 * to the value as "name=value". If $required is set to true and the
-		 * offset is not set, an exception will be thrown.
-		 *
-		 * @param mixed $offset
-		 * @param bool $prefix
-		 * @param bool $required
-		 * @return mixed|string
-		 * @throws \Exception
-		 */
-		public function offsetGet($offset, $prefix = false, $required = false)
-		{
-			$value = '';
-
-			if (is_array($offset)) {
-				if ($prefix === true) {
-					$prefix = $offset[0].'=';
-				}
-
-				foreach ($offset as $item) {
-					if ($this->offsetExists($item)) {
-						$offset = $item;
-						break;
-					}
-				}
-			} elseif ($prefix === true) {
-				$prefix = $offset.'=';
-			}
-
-			if ($this->offsetExists($offset)) {
-				$value = $prefix.$this->_config[$offset];
-			} elseif ($required) {
-				throw new \Exception();
-			}
-
-			return $value;
-		}
-
-		/**
-		 * Set a value to the config name.
-		 *
-		 * @param mixed $offset
-		 * @param mixed $value
-		 */
-		public function offsetSet($offset, $value)
-		{
-			$this->_config[$offset] = $value;
-		}
-
-		/**
-		 * Unset the offset from the config.
-		 *
-		 * @param mixed $offset
-		 */
-		public function offsetUnset($offset)
-		{
-			unset($this->_config[$offset]);
 		}
 
 		/**
--- tests/Database/DsnTest.php
+++ tests/Database/DsnTest.php
@@ -12,39 +12,6 @@ class DBDsnTest extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @covers \SiTech\Database\Dsn::__construct
-	 */
-	public function testConstruct()
-	{
-		$dsn = new DBDsnTestMockDsn(['foo' => 'bar']);
-		$this->assertArrayHasKey('foo', $this->readAttribute($dsn, '_config'));
-	}
-
-	/**
-	 * @covers \SiTech\Database\Dsn::__get
-	 */
-	public function testMagicGet()
-	{
-		$this->assertEquals('bar', $this->_dsn->foo);
-		$this->assertNull($this->_dsn->isnotset);
-	}
-
-	public function testMagicSet()
-	{
-		$this->_dsn->bar = 'baz';
-		$this->assertEquals('baz', $this->readAttribute($this->_dsn, '_config')['bar']);
-	}
-
-	/**
-	 * @covers \SiTech\Database\Dsn::__isset
-	 */
-	public function testMagicIsset()
-	{
-		$this->assertTrue(isset($this->_dsn->foo));
-		$this->assertFalse(isset($this->_dsn->iamnotset));
-	}
-
-	/**
 	 * @covers \SiTech\Database\Dsn::getDsn
 	 * @covers \SiTech\Database\Dsn::__toString
 	 */
@@ -53,48 +20,6 @@ class DBDsnTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('dsn', $this->_dsn->getDsn());
 		// this returns a call to getDsn, so if the previous fails, this will to... meh
 		$this->assertEquals('dsn', (string)$this->_dsn);
-	}
-
-	/**
-	 * @covers \SiTech\Database\Dsn::offsetGet
-	 * @expectedException \Exception
-	 */
-	public function testOffsetGet()
-	{
-		$this->assertEquals('bar', $this->_dsn->offsetGet('foo'));
-		$this->assertEquals('bar', $this->_dsn->offsetGet(['foo', 'fooey']));
-		$this->assertEquals('foo=bar', $this->_dsn->offsetGet('foo', true));
-		$this->assertEquals('foo=bar', $this->_dsn->offsetGet(['foo', 'fooey'], true));
-		$this->assertEmpty($this->_dsn->offsetGet('bar'));
-		$this->_dsn->offsetGet('bar', false, true);
-	}
-
-	/**
-	 * @covers \SiTech\Database\Dsn::offsetExists
-	 */
-	public function testOffsetExists()
-	{
-		$this->assertTrue($this->_dsn->offsetExists('foo'));
-		$this->assertFalse($this->_dsn->offsetExists('bar'));
-	}
-
-	/**
-	 * @covers \SiTech\Database\Dsn::offsetSet
-	 */
-	public function testOffsetSet()
-	{
-		$this->_dsn->offsetSet('shebang', 'yup');
-		$this->assertEquals('yup', $this->_dsn->offsetGet('shebang'));
-	}
-
-	/**
-	 * @covers \SiTech\Database\Dsn::offsetUnset
-	 */
-	public function testOffsetUnset()
-	{
-		$this->_dsn->offsetSet('unset', 'yup');
-		$this->_dsn->offsetUnset('unset');
-		$this->assertEmpty($this->_dsn->offsetGet('unset'));
 	}
 }
 
