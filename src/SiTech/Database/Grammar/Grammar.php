dbd6e521e6c2d019ff95b901bd168c9fc64305a6
Eric Gach <eric@php-oop.net>
2014-07-21 16:22:45 -0500

    adding more documentation

--- src/SiTech/Database/Grammar/Grammar.php
+++ src/SiTech/Database/Grammar/Grammar.php
@@ -1,13 +1,42 @@
 <?php
+/**
+ * Copyright (c) 2014 Eric Gach <eric@php-oop.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author Eric Gach <eric@php-oop.net>
+ * @copyright Copyright (c) 2014 Eric Gach <eric@php-oop.net>
+ * @license MIT
+ * @package SiTech\Database
+ * @subpackage SiTech\Database\Grammar
+ */
+
 namespace SiTech\Database\Grammar
 {
 	/**
 	 * This is the base grammar class that all database grammars will extend.
 	 *
-	 * This class provides the basic neccisary grammar functions that all
+	 * This class provides the basic necessary grammar functions that all
 	 * grammar classes will need to either use or override.
 	 *
-	 * @package SiTech\Database\Grammar
+	 * @package SiTech\Database
+	 * @subpackage SiTech\Database\Grammar
 	 */
 	abstract class Grammar
 	{
@@ -57,9 +86,10 @@ namespace SiTech\Database\Grammar
 			$out = [];
 			$parts = explode('.', $value);
 
+			// now wrap each part of the value
 			foreach ($parts as $k => $part) {
 				// If we have a table.column value, we need to add the prefix to
-				// the table value.
+				// the first part of the value, the table
 				if ($k === 0 && sizeof($parts) > 1) {
 					$part = $this->prefixTable($part);
 				}
